ddcdf2664e3641adcaa70949fb7e3f644f5e4ef5
Anton <anton.shkurdov@vrpconsulting.com>
2022-09-27 16:01:21 +0300

    updated pages

--- helpers/db.js
+++ helpers/db.js
@@ -3,9 +3,9 @@ const { Pool } = require('pg')
  const dbСredits = {
     host: 'postgres',
     port: 5432,
-    database: 'habrdb',
-    user: 'habrpguser',
-    password: 'pgpwd4habr',
+    database: 'shkurdovdb',
+    user: 'shkurdovuser',
+    password: 'pgpwd4shkurdov',
     max: 50,
     idleTimeoutMillis: 10000,
     connectionTimeoutMillis: 10000
