fc515eb022fe3deed97e9b6bce31abe2ce2d3a02
Anton <anton.shkurdov@vrpconsulting.com>
2022-09-30 15:44:48 +0300

    updated contacts page

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 
 const express = require('express');
 const cors = require('cors');
-const morgan = require('morgan');
 const methodOverride = require('method-override');
 const postRoutes = require('./routes/post-routes');
 const postApiRoutes = require('./routes/api-post-routes');
